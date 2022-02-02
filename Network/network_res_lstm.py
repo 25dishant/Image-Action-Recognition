c792f9cb21b91720fc96a548461397d4cafbbee6
Dishant Satuley <25dishant@gmail.com>
2022-02-02 23:58:59 +0530

    stacked all rois togethor

--- Network/network_res_lstm.py
+++ Network/network_res_lstm.py
@@ -31,39 +31,55 @@ class Custom_Model(torch.nn.Module):
 
         """
         pdb.set_trace()
-        # obj_box = obj_box.reshape((-1, 4))
-        obj_box = list(obj_box)
-        gt_box = list(gt_box)
+        #obj_box = obj_box.reshape((-1, 4))
+        # obj_box = list(obj_box)
+        # gt_box = list(gt_box)
         print("gt_box: ",gt_box)
         print("obj_box: ",obj_box)
         print("fixations: ",fixations)
+        gt_box = gt_box.reshape(gt_box.shape[1],gt_box.shape[2])
+        obj_box = obj_box.reshape(obj_box.shape[1],obj_box.shape[2])
+        all_rois = torch.row_stack((gt_box,obj_box))
+        all_rois = all_rois.reshape(1,all_rois.shape[0],all_rois.shape[1])
+        print(all_rois)
+
+        all_rois = list(all_rois)
+        print(all_rois)
         fourth_layer_output = self.model1(x)
         # pooled_feat = torchvision.ops.roi_pool(fourth_layer_output, gt_box, output_size=(14, 14), spatial_scale=0.0625)
         # pooled_ctx_feat = torchvision.ops.roi_pool(fourth_layer_output, obj_box, output_size=(14, 14), spatial_scale=0.0625)
-        pooled_feat = torchvision.ops.roi_align(fourth_layer_output, gt_box, output_size=(14, 14), spatial_scale=0.0625,sampling_ratio=2)
-        pooled_ctx_feat = torchvision.ops.roi_align(fourth_layer_output, obj_box, output_size=(14, 14), spatial_scale=0.0625,sampling_ratio=2)
+        # pooled_feat = torchvision.ops.roi_align(fourth_layer_output, gt_box, output_size=(14, 14), spatial_scale=0.0625,sampling_ratio=2)
+        # pooled_ctx_feat = torchvision.ops.roi_align(fourth_layer_output, obj_box, output_size=(14, 14), spatial_scale=0.0625,sampling_ratio=2)
+        pooled_feat = torchvision.ops.roi_align(fourth_layer_output, all_rois, output_size=(14, 14), spatial_scale=0.0625,sampling_ratio=2)
         # pooled_feat = pooled_feat[0,:,:,:].reshape(1,1024,14,14)
         # pooled_ctx_feat = pooled_ctx_feat[0,:,:,:].reshape(1,1024,14,14)
-        top_feat = self.model2(pooled_feat)
-        top_ctx_feat = self.model2(pooled_ctx_feat)
+        # top_feat = self.model2(pooled_feat)
+        # top_ctx_feat = self.model2(pooled_ctx_feat)
+        features = self.model2(pooled_feat)
         print("CONTROL IS HERE")
         
         # top_feat = self.global_avg_pool(pooled_feat)
         # top_ctx_feat = self.global_avg_pool(pooled_ctx_feat)
-        top_feat = top_feat.flatten(1,3)
-        top_ctx_feat = top_ctx_feat.flatten(1,3)
+        # top_feat = top_feat.flatten(1,3)
+        # top_ctx_feat = top_ctx_feat.flatten(1,3)
+        features = features.flatten(1,3)
         
-        top_feat = self.fc1(top_feat)
-        top_ctx_feat = self.fc1(top_ctx_feat)
+        # top_feat = self.fc1(top_feat)
+        # top_ctx_feat = self.fc1(top_ctx_feat)
+        features = self.fc1(features)
 
         hidden = (torch.zeros(1, 1, 512).to(self.device), torch.zeros(1, 1, 512).to(self.device))
         # hidden = hidden.to(self.device)
-        for t1 in top_feat:
-            t1 = t1.reshape(1,1,1024)
-            out1, hidden = self.model3(t1,hidden)
-            for t2 in top_ctx_feat:
-                t2 = t2.reshape(1,1,1024)
-                out, hidden = self.model3(t2,hidden)
+        # for t1 in top_feat:
+        #     t1 = t1.reshape(1,1,1024)
+        #     out1, hidden = self.model3(t1,hidden)
+        #     for t2 in top_ctx_feat:
+        #         t2 = t2.reshape(1,1,1024)
+        #         out, hidden = self.model3(t2,hidden)
+
+        for feat in features:
+            feat = feat.reshape(1,1,1024)
+            out, hidden = self.model3(feat,hidden)
 
         output = self.fc2(out)    
         print(output)
